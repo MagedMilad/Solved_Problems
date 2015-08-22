2541e36c3624d072db3ee9801955071ae3c329d4
magedmilad <magedmilad08@gmail.com>
2015-08-22 21:52:47 +0300

    Spoj

---
+++ Spoj/PARTY - Party Schedule.cpp
@@ -0,0 +1,116 @@
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <iostream>
+#include <iomanip>
+#include <complex>
+
+using namespace std;
+
+typedef stringstream ss;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef vector<vector<ii> > vvii;
+typedef vector<string> vs;
+typedef vector<int> vi;
+typedef vector<vector<int> > vvi;
+typedef vector<double> vd;
+typedef long double ld;
+typedef vector<vector<ll> > grix;
+typedef complex<double> point;
+typedef complex<long long> pointINT;
+
+int dx[] = { -1, 1, 0, 0, 0, 0 };
+int dy[] = { 0, 0, -1, 1, 0, 0 };
+int dz[] = { 0, 0, 0, 0, -1, 1 };
+
+int DX[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
+int DY[] = { 1, -1, 0, 0, 1, -1, -1, 1 };
+
+double PI = 3.1415926535897932384626433832795;
+
+const ll oo = 1e9 + 1;
+const double eps = 1e-6;
+const ll mod = 1234567;
+
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define sz(v) ((int)v.size())
+#define clr(v, d) memset(v, d, sizeof(v))
+#define polar(r,t) ((r)*exp(point(0,(t))))
+#define length(a) hypot((a).real(),(a).imag())
+#define angle(a) atan2((a).imag() , (a).real())
+#define vec(a,b) ((b)-(a))
+#define dot(a,b) ((conj(a)*(b)).real())
+#define cross(a,b) ((conj(a)*(b)).imag())
+#define lengthSqr(a) dot(a,a)
+#define rotate(v,t) ((v)*exp(point(0,t)))
+#define rotateAbout(v,t,a) (rotate(vec(a,v),t)+(a))
+#define reflect(v,m) (conj((v)/(m))*m)
+#define dist(a,b) (sqrt(pow((a).real()-(b).real(),2.0)+pow((a).imag()-(b).imag(),2.0)))
+#define distsqr(a,b) (pow((a).real()-(b).real(),2.0)+pow((a).imag()-(b).imag(),2.0))
+#define normalize(a) ((a)/length(a))
+#define ccw(a,b,c) (cross(vec((a),(b)),vec((a),(c)))>=0)
+#define collinear(a,b,c) (fabs(cross(vec((a),(b)),vec((a),(c))))<eps)
+#define LSOne(S) (S & (-S))
+////////////////////////////////////////////////////////////////////////
+
+//freopen("in.txt","r",stdin);
+//freopen("out.txt","w",stdout);
+
+int cost[101], fun[101], have, n;
+int memo[101][501];
+
+int solve(int idx, int have) {
+	if (idx == n) {
+		if (have < 0)
+			return -oo;
+		return 0;
+	}
+	if (have < 0)
+		return -oo;
+	if (memo[idx][have] != -1)
+		return memo[idx][have];
+	return memo[idx][have] = max(solve(idx + 1, have),
+			solve(idx + 1, have - cost[idx]) + fun[idx]);
+}
+
+int main() {
+//	freopen("in.txt","r",stdin);
+	while (scanf("%d %d", &have, &n)) {
+		if(have == 0 && n == 0)
+			break;
+		clr(memo,-1);
+		for (int i = 0; i < n; i++)
+			scanf("%d %d", &cost[i], &fun[i]);
+		int ret = solve(0, have);
+		int lo = 0, hi = have;
+		while (lo < hi) {
+			int mi = lo + (hi - lo) / 2;
+			if (solve(0, mi) < ret)
+				lo = mi + 1;
+			else
+				hi = mi;
+		}
+		printf("%d %d\n", lo, ret);
+	}
+	return 0;
+}
+
